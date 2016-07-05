85e6aae4902831d2d7bccd65bfea0930c747cb40
xeonos <elie@el-khoury.net>
2016-07-05 19:45:28 +0200

    bootstrap theme: https://bootswatch.com/paper/

--- app/bookmarks-local.ts
+++ app/bookmarks-local.ts
@@ -7,7 +7,12 @@ import { Bookmark,BookmarkType } from './bookmark';
 @Component({
     selector: 'bookmarks',
     template: `
-        <p>number of bookmaks: {{values.length}}</p>
+    <ul class="list-group">
+        <li *ngFor="let b of values" class="list-group-item">
+                        <img src=""/>&nbsp;
+                        <a title="{{b.title}}" href="{{b.url}}" target="_blank">{{b.title}}</a>
+        </li>
+    </ul>
     `
 })
 
@@ -16,29 +21,35 @@ export class BookmarksLocal {
     public values:Array<Bookmark>;
 
     constructor() {
-        this.values = new Array<Bookmark>();
+        this.values = this.retrieveBookmarks();
+    }
+
+    private retrieveBookmarks():Array<Bookmark> {
+        let result = new Array<Bookmark>();
         chrome.bookmarks.getTree(
             function (bookmarkTreeNodes) {
                 if (bookmarkTreeNodes[0].children && bookmarkTreeNodes[0].children[0].children) {
                     for (let i = 0; i < bookmarkTreeNodes[0].children[0].children.length; i++) {
                         let level0 = bookmarkTreeNodes[0].children[0].children[i];
-                        this.scanLocalBookmarks(level0, []);
+                        BookmarksLocal.prototype.scanLocalBookmarks(level0, [], result);
                     }
                 }
             }
         );
+
+        return result;
     }
 
-    scanLocalBookmarks(bookmarkNode:any, parentTags:any) {
+    private scanLocalBookmarks(bookmarkNode:any, parentTags:any, result:Array<Bookmark>):void {
         if (bookmarkNode.url != undefined) {
             let bookmark = new Bookmark(bookmarkNode.url, bookmarkNode.title, parentTags, BookmarkType.LOCAL, '#91205a');
 
-            this.values.push(bookmark);
+            result.push(bookmark);
         } else if (bookmarkNode.children != undefined) {
             let tempTag = parentTags.slice();
             tempTag.push(bookmarkNode.title);
             for (var n = 0; n < bookmarkNode.children.length; n++) {
-                this.scanLocalBookmarks(bookmarkNode.children[n], tempTag);
+                this.scanLocalBookmarks(bookmarkNode.children[n], tempTag, result);
             }
         }
     }
