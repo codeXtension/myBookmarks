1ab631a598570eb06803b10f99c9f8d55633778e
Elie Khoury <elie@el-khoury.net>
2016-10-18 19:40:05 +0200

    layout changes

--- bookmarks.config.js
+++ bookmarks.config.js
@@ -3,4 +3,10 @@ $(function(){
         function(err) {
             console.error(err.message);
         });
+
+/*    $(window).resize(function(){
+        $(".bookmark-scrollable").height(screen.height-130);
+                console.info('setting on resize .bookmark-scrollable to ' + (screen.height-130));
+
+    });*/
 });
