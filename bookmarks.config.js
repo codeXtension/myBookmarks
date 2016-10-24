c33adcdd2986f6a75f933acce12cd8f1770225fb
Elie el Khoury <elie.el-khoury@t-systems.com>
2016-10-24 07:23:03 +0200

    css clean up

--- bookmarks.config.js
+++ bookmarks.config.js
@@ -3,14 +3,4 @@ $(function(){
         function(err) {
             console.error(err.message);
         });
-
-    $(window).resize(function(){
-        $(".bookmark-scrollable").height(window.innerHeight);
-                console.info('setting on resize .bookmark-scrollable to ' + (window.innerHeight));
-
-    });
-
-    window.setTimeout(function() {
-        $(".bookmark-scrollable").height(window.innerHeight);
-    }, 500);
 });
