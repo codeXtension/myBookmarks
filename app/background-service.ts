7a9b00da7a74df3bed3a6d29956f9d9024e81a89
Elie Khoury <elie@el-khoury.net>
2016-12-09 13:14:18 +0100

    making the plugin use local bookmarks only

--- app/background-service.ts
+++ app/background-service.ts
@@ -4,33 +4,55 @@
 
 import { Component, OnInit,Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {LocalBookmarkResolver} from "./local/local-bookmark-resolver";
+import {Bookmark} from "./bookmark";
+import SuggestResult = chrome.omnibox.SuggestResult;
 
 @Component({
     selector: 'background',
+    providers: [LocalBookmarkResolver],
     template: ''
 })
 export class BackgroundService implements OnInit {
 
+    constructor(private localBookmarkResolver:LocalBookmarkResolver) {
+
+    }
+
     ngOnInit() {
-        chrome.omnibox.onInputStarted.addListener(function() {
-          chrome.omnibox.setDefaultSuggestion({
-            description: 'Search for %s'
-          });
+        let bookmarksFinder:LocalBookmarkResolver = this.localBookmarkResolver;
+
+        chrome.omnibox.onInputStarted.addListener(function () {
+            chrome.omnibox.setDefaultSuggestion({
+                description: 'Searching the bookmarks for %s'
+            });
         });
 
         chrome.omnibox.onInputChanged.addListener(
-          function(text, suggest) {
-            console.info('Text for search' + text);
-          });
+            function (text, suggest:any) { // SuggestResult[]
+                let filteredValues:Array<Bookmark> = [];
+                console.info('Text for search ' + text);
+                bookmarksFinder.find(text).then(bookmarks => {
+                    filteredValues = bookmarks;
+                    let results:SuggestResult[] = [];
+                    for (let fv of filteredValues) {
+                        let res:SuggestResult = {content: null, description: null};
+                        res.description = fv.title;
+                        res.content = fv.url;
+                        results.push(res);
+                    }
+                    suggest(results);
+                });
+            });
 
         chrome.omnibox.onInputEntered.addListener(
-            function(text:string) {
-              chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.update(tabs[0].id, {url: text});
-              });
-          });
+            function (text:string) {
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.update(tabs[0].id, {url: text});
+                });
+            });
 
-        chrome.omnibox.onInputCancelled.addListener(function() {
+        chrome.omnibox.onInputCancelled.addListener(function () {
 
         });
     }
